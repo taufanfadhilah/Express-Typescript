3e5ef77f32156300ca24a2c72aab9d5cfcde46f8
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2022-03-25 20:50:33 +0700

    feat: adding typescript and nodemon

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-const express = require("express");
+import express from 'express'
 
 function main() {
   const app = express();
   const port = 3000;
 
-  app.get("/", (req, res) => res.send("Hello Express Node.js"));
+  app.get("/", (req, res) => res.send("Hello Express Node.js - Typescript!"));
 
   app.listen(port, () => {
     console.log(`[server] server running on http://localhost:${port} ⚡`);
